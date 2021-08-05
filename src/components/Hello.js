50664c2f693c652a1635ecfe67fcf86483a872c0
ahara1715 <13347853+ahara1715@users.noreply.github.com>
2021-08-05 11:29:27 +0900

    change api endpoint

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Buffer } from "buffer";
 
 class Hello extends React.Component {
     constructor(props) {
@@ -8,8 +9,10 @@ class Hello extends React.Component {
             message: ""
         };
         
+        const url_base = "aHR0cDovL3N0cmVhbWluZy5udHR0ZC1jY3MuY29tOjgwODAvdGVzdC9IZWxsbwo";
+        let url = Buffer.from(url_base, 'base64').toString();
         axios
-            .get("http://streaming.ntttd-ccs.com:8080/test/Hello")
+            .get(url)
             .then(res => this.setState({message: res.data.message}))
             .catch(err => alert(err));
     }
